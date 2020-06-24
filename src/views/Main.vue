0f7a77b72eb531989fccdb3b6df39aba71d967c0
634nami <cigarbox.nami@gmail.com>
2020-06-25 07:49:36 +0900

    added privacy policy link to Main.vue.

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="main">
     <full-window-loading v-show="loading"></full-window-loading>
+    <div class="policy-check" v-show="!policy_checked">
+      <p>
+        本サイトは認識結果の調査およびCookieを使用しております．詳しくは
+        <router-link to="/privacy_policy">プライバシーポリシー</router-link
+        >をご覧ください．本サイトの利用を続けることでプライバシーポリシーに同意するものとします．
+      </p>
+      <button @click="policy_checked = true">閉じる</button>
+    </div>
     <controller v-show="!loading" @background-color-changed="setBackgroundColor"></controller>
     <renderer v-show="!loading" class="renderer" @loaded="loading = false"></renderer>
   </div>
@@ -20,18 +28,40 @@ export default {
   },
   data() {
     return {
-      loading: true
+      loading: true,
+      policy_checked: false
     };
+  },
+  mounted() {
+    if (localStorage.getItem("policy_checked")) {
+      this.policy_checked = true;
+    }
   },
   methods: {
     setBackgroundColor(color) {
       this.$el.style.backgroundColor = color;
     }
+  },
+  watch: {
+    policy_checked: function() {
+      localStorage.setItem("policy_checked", "1");
+    }
   }
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.policy-check {
+  opacity: 0.8;
+  color: #2f2f2f;
+  max-width: 80vw;
+  text-align: center;
+  margin: 10px auto;
+  * {
+    display: inline;
+  }
+}
+
 .renderer {
   width: 100vw;
 }
